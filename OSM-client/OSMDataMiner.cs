f64decddbd01cbcedb3d59aa908c8ce1c0ae7914
d21eagle <vlad180317@gmail.com>
2023-10-13 19:22:17 +0600

    *Добавлен проект OSM-client

---
+++ OSM-client/OSMDataMiner.cs
@@ -0,0 +1,65 @@
+﻿using OsmSharp;
+using OsmSharp.Streams;
+
+namespace OSM_client;
+
+public class OsmDataMiner
+{
+    public void ParseOsmData(string filePath)
+    {
+        
+        var fileStream = new FileInfo(filePath).OpenRead();
+        // var source = new PBFOsmStreamSource(fileStream);
+            
+        var osmStreamSource = new XmlOsmStreamSource(fileStream);
+        var nodes = new List<Node>();
+        var ways = new List<Way>();
+        var relations = new List<Relation>();
+        
+        foreach (var element in osmStreamSource)
+        {
+            if (element.Type is OsmGeoType.Node)
+            {
+                nodes.Add((Node) element);
+            }
+            
+            else if (element.Type is OsmGeoType.Way)
+            {
+                ways.Add((Way) element);
+            }
+            
+            else if (element.Type is OsmGeoType.Relation)
+            {
+                relations.Add((Relation) element);
+            }
+        }
+
+        var streets = GetStreetList(ways);
+        foreach (var street in streets)
+        {
+            Console.WriteLine("\nГеометрия улицы c Id: " + street.Id);
+            foreach (var nodeId in street.Nodes)
+            {
+                foreach (var node in nodes.Where(node => node.Id == nodeId))
+                {
+                    Console.WriteLine("Координаты: Lat = " + node.Latitude + ", Lon = " + node.Longitude);
+                }
+            }
+        }
+    }
+
+    private List<Way> GetStreetList(List<Way> ways)
+    {
+        var streets = new List<Way>();
+        foreach (var way in ways)
+        {
+            if (way.Tags.ContainsKey("highway") && way.Tags["highway"] == "residential")
+            {
+                if (way.Tags.ContainsKey("name"))
+                    streets.Add(way);
+            }   
+        }
+
+        return streets;
+    }
+}
---
+++ OSM-client/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace OSM_client
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var osmMiner = new OsmDataMiner();
+            var filePath = "../../../overpass_data.xml";
+            osmMiner.ParseOsmData(filePath);
+        }
+    }
+}
