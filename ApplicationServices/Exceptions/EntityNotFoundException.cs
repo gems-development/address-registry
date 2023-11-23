188b9ad71a0090599def857795bb432bca993ec3
user <user@LAPTOP-9O2R9AD2>
2023-11-23 11:34:54 +0600

    Исправление опечатки

--- ApplicationServices/Exceptions/EntityNotFoundException.cs
+++ ApplicationServices/Exceptions/EntityNotFoundException.cs
@@ -5,7 +5,7 @@ namespace AplicationServices.Exceptions;
 public class EntityNotFoundException: Exception
 {
     public EntityNotFoundException(): base() { }
-    public EntityNotFoundException(Guid guid) : base($"Entity with id: {guid} not found") { }
+    public EntityNotFoundException(Guid guid) : base($"Entity with id: {guid} is not found") { }
     
     
 }
