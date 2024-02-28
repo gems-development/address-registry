ac39ea48230efee2084ec4ee3ac0b29b265ba4aa
d21eagle <vlad180317@gmail.com>
2024-02-28 15:40:29 +0600

    *правка свойства

--- OsmDataParser/Model/RealObject.cs
+++ OsmDataParser/Model/RealObject.cs
@@ -4,6 +4,6 @@ namespace Gems.AddressRegistry.OsmDataParser.Model;
 
 public class RealObject
 {
-    public string Name { get; init; }
+    public string Name { get; init; } = null!;
     public ICollection<Way> Components { get; set; } = new List<Way>();
 }
