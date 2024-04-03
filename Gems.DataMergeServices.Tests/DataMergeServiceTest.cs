03516ea741a012adf9d85e9c4856d681bd1c3df6
d21eagle <vlad180317@gmail.com>
2024-04-03 16:06:00 +0600

    *убрал лишнюю строку

--- Gems.DataMergeServices.Tests/DataMergeServiceTest.cs
+++ Gems.DataMergeServices.Tests/DataMergeServiceTest.cs
@@ -91,8 +91,6 @@ public class DataMergeServiceTest
             house1
         };
 
-        NormalizedAddressBuilder.BuildAddress(addressesOsm);
-
         var addressesFias = new List<Address>();
 
         var building1 = new Building();
