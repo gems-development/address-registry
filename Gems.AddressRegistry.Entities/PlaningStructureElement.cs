0d495b63bcd273783d05a38a0402cf99a8775331
d21eagle <vlad180317@gmail.com>
2024-04-26 11:34:58 +0600

    +ссылки на род элементы

--- Gems.AddressRegistry.Entities/PlaningStructureElement.cs
+++ Gems.AddressRegistry.Entities/PlaningStructureElement.cs
@@ -6,6 +6,8 @@ namespace Gems.AddressRegistry.Entities
     public class PlaningStructureElement : BaseGeoEntity
     {
         public virtual ICollection<EpsDataSource> DataSources { get; set; } = new List<EpsDataSource>(0);
+        public City? City { get; set; }
+        public Settlement? Settlement { get; set; }
         public String Name { get; set; } = null!;
     }
 }
