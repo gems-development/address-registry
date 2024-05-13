76e9fb5d459b5b8b722ac8102637a4ee3cffd300
Denis <Kds04042002omgu@gmail.com>
2024-05-13 21:18:55 +0600

    фикс слияния

--- OsmDataParser/Support/OsmKeywords.cs
+++ OsmDataParser/Support/OsmKeywords.cs
@@ -1,4 +1,4 @@
-namespace osmDataParser;
+namespace Gems.AddressRegistry.OsmDataParser.Support;
 
 internal static class OsmKeywords
 {
@@ -7,7 +7,17 @@ internal static class OsmKeywords
     public const string Name = "name";
     public const string Highway = "highway";
     public const string Administrative = "administrative";
-    
+    public const string Place = "place";
+
+    public const string Building = "building";
+    public const string StreetName = "addr:street";
+    public const string HouseNumber = "addr:housenumber";
+
+    public const string City = "city";
+    public const string Town = "town";
+    public const string Village = "village";
+    public const string Hamlet = "hamlet";
+
     public const string Level2 = "2";
     public const string Level4 = "4";
     public const string Level6 = "6";
