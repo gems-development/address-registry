a1b97b2b7d9c9df557f071ca75a9727a7240e5bc
Дмитрий Долгушин <ddolgushin@gemsdev.com>
2024-06-07 14:44:10 +0600

    - Лишнее.

--- OsmDataParser/DataGroupingServices/ObjectIntersector.cs
+++ OsmDataParser/DataGroupingServices/ObjectIntersector.cs
@@ -1,4 +1,3 @@
-using System.Collections.Concurrent;
 using NetTopologySuite.Features;
 using NetTopologySuite.IO;
 using Gems.AddressRegistry.OsmDataParser.Model;
@@ -7,31 +6,14 @@ namespace Gems.AddressRegistry.OsmDataParser.DataGroupingServices;
 
 public static class ObjectIntersector
 {
-	private static readonly ConcurrentDictionary<string, FeatureCollection> _cachedGeometries;
 	private static readonly GeoJsonReader _geoJsonReader;
 
-	static ObjectIntersector()
-	{
-		_cachedGeometries = new ConcurrentDictionary<string, FeatureCollection>();
-		_geoJsonReader = new GeoJsonReader();
-	}
+	static ObjectIntersector() => _geoJsonReader = new GeoJsonReader();
 
 	public static bool Intersects(RealObject realObject1, RealObject realObject2)
 	{
-		if (!_cachedGeometries.TryGetValue(realObject1.GeoJson, out var firstGeometry))
-		{
-			firstGeometry = _geoJsonReader.Read<FeatureCollection>(realObject1.GeoJson);
-
-			_cachedGeometries.AddOrUpdate(realObject1.GeoJson, firstGeometry, (_, collection) => collection);
-		}
-
-		if (!_cachedGeometries.TryGetValue(realObject1.GeoJson, out var secondGeometry))
-		{
-			secondGeometry = _geoJsonReader.Read<FeatureCollection>(realObject2.GeoJson);
-
-			_cachedGeometries.AddOrUpdate(realObject2.GeoJson, secondGeometry, (_, collection) => collection);
-		}
-
+		var firstGeometry = _geoJsonReader.Read<FeatureCollection>(realObject1.GeoJson);
+		var secondGeometry = _geoJsonReader.Read<FeatureCollection>(realObject2.GeoJson);
 		var firstFeature = firstGeometry?.FirstOrDefault();
 		var secondFeature = secondGeometry?.FirstOrDefault();
 
